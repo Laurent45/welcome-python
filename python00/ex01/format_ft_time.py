79b705509e0b88afbc37eb6b5cf544aafa2ebd5a
laurentfrederick <laurent.frederick@icloud.com>
2023-12-20 23:32:58 +0100

    python00 ex01

---
+++ python00/ex01/format_ft_time.py
@@ -0,0 +1,20 @@
+"""
+    The purpose of this exercice is to get the current time and parse it.
+    The output will look like this:
+
+    $>python format_ft_time.py | cat -e
+    Seconds since January 1, 1970: 1,666,355,857.3622 or 1.67e+09 in scientific notation$
+    Oct 21 2022$
+    $>
+
+    About standard format specifier: https://docs.python.org/3/library/string.html#formatspec 
+"""
+
+from time import time
+from time import strftime
+from time import localtime
+
+currentTime = time()
+
+print(f"Seconds since January 1, 1970: {currentTime:,.4f} or {currentTime:#.3} in scientific notation")
+print(strftime("%b %d %Y", localtime(currentTime)))
