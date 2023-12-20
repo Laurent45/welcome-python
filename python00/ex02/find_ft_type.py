112cf8a47eaa7c72eb9758c42475a2cfcbdaf3bc
laurentfrederick <laurent.frederick@icloud.com>
2023-12-21 00:25:29 +0100

    python00 ex02

---
+++ python00/ex02/find_ft_type.py
@@ -0,0 +1,25 @@
+"""
+    The purpose of this exercise is to implement a function
+    which print the type of parameter
+
+"""
+
+def all_thing_is_obj(object: any) -> int:
+    #your code here
+
+    objType = type(object)
+
+    if objType == list:
+        print("List :", objType)
+    elif objType == tuple:
+        print("Tuple :", objType)
+    elif objType == dict:
+        print("Dict :", objType)
+    elif objType == set:
+        print("Set :", objType)
+    elif objType == str:
+        print(f"{object} is in the kitchen : {objType}")
+    else:
+        print("Type not found")
+
+    return 42
