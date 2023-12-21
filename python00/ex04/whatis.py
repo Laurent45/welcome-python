7d5180bc4e2bc69d44acddccbc1c585de2f69c15
laurentfrederick <laurent.frederick@icloud.com>
2023-12-21 16:58:05 +0100

    python00 ex04

---
+++ python00/ex04/whatis.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+args = len(argv)
+
+if (args > 2):
+    print("AssertionError: more than one argument is provided")
+elif args == 2:
+    try:
+        whatis = "Even" if int(argv[1]) % 2 == 0 else "Odd"
+        print(f"I am {whatis}")
+    except ValueError:
+        print("AssertionError: argument is not an integer")
