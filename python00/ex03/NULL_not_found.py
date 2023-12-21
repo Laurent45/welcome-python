e78c83677bf2273702ded59930094ade6b9eea15
laurentfrederick <laurent.frederick@icloud.com>
2023-12-21 16:21:41 +0100

    python00 ex03

---
+++ python00/ex03/NULL_not_found.py
@@ -0,0 +1,22 @@
+
+def NULL_not_found(object: any) -> int:
+    #your code here
+
+    objectType = type(object)
+    ret = 0
+
+    if objectType == None:
+        print(f"Nothing: {object} {objectType}")
+    elif objectType == float:
+        print(f"Cheese: {object} {objectType}")
+    elif objectType == int:
+        print(f"Zero: {object} {objectType}")
+    elif objectType == str and object == "":
+        print(f"Empty: {objectType}")
+    elif objectType == bool:
+        print(f"Fake: {object} {objectType}")
+    else:
+        print("Type not found")
+        ret = 1
+
+    return ret
