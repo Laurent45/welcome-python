811c570adf3d69b1ac78b2adf141d5d480711f5d
laurentfrederick <laurent.frederick@icloud.com>
2023-12-21 15:27:39 +0100

    refacto ex00

--- python00/ex00/Hello.py
+++ python00/ex00/Hello.py
@@ -1,16 +1,4 @@
 
-"""
-    The purpose of this exercise is to write some code in order to output
-    look like this:
-
-    >python Hello.py | cat -e
-    ['Hello', 'World!']$
-    ('Hello', 'France!')$
-    {'Hello', 'Paris!'}$
-    {'Hello': '42Paris!'}$
-    >
-"""
-
 ft_list = ["Hello", "tata!"]
 ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
@@ -20,11 +8,11 @@ ft_dict = {"Hello" : "titi!"}
 
 ft_list[1] = "World!"
 
-# You must construct an other tuple because tuple is immutable
+# Assign an other tuple because tuple is immutable
 ft_tuple = (ft_tuple[0], "France!")
 
-# Sets are unordered, so to keep the correct order about this exercise,
-# you can assign a string to ft_set variable
+# In order to keep the correct order about the expected output,
+# I assign a string to ft_set variable
 ft_set = "{'Hello', 'Paris!'}"
 
 ft_dict["Hello"] = "42Paris!"
