f0fcae500b461e7ae180b3cf656fba98e08cff0e
Laurent FREDERICK <laurent.frederick@icloud.com>
2024-01-19 15:07:56 +0100

    replace global variable into function

--- python00/ex08/Loading.py
+++ python00/ex08/Loading.py
@@ -3,18 +3,17 @@
 
 """
 
-BAR_SIZE = 180
-FILL_CHAR = "="
-SPACE_CHAR = " "
-
 
 def ft_tqdm(lst: range) -> None:
     # your code here
+    bar_size = 180
+    fill_char = "="
+    space_char = " "
     total = len(lst)
 
     for i in range(0, total):
         percentage = i * 100 // total
-        fill_rate = BAR_SIZE * percentage // 100
-        print(f"{percentage}%|[{FILL_CHAR * fill_rate}{SPACE_CHAR * (BAR_SIZE - fill_rate)}]| {i}/{total}", end="\r")
+        fill_rate = bar_size * percentage // 100
+        print(f"{percentage}%|[{fill_char * fill_rate}{space_char * (bar_size - fill_rate)}]| {i}/{total}", end="\r")
         yield
-    print(f"100%|[{FILL_CHAR * (BAR_SIZE - 1)}>]| {total}/{total}", end="")
+    print(f"100%|[{fill_char * (bar_size - 1)}>]| {total}/{total}", end="")
